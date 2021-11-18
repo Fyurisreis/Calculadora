da91afb6f0eeae94d57455a2c537f07732c29f9f
Fyurisreis <fernandoysreis@gmail.com>
2021-11-18 17:01:01 -0300

    Sistema de Gerenciamento

---
+++ primeiro-projeto-java/exemplo-gradle/src/test/java/br/com/autonomo/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.autonomo.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
